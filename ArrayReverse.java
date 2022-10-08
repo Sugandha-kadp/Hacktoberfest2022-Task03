ed6c057c0d0725e6725fad03d897a3bcb7662361
ABHISHEK KUMAR JHA <55275135+diabl0-NEMESIS@users.noreply.github.com>
2022-10-08 19:58:24 +0530

    ArrayReverse,java
    Updated the code with the correct one.

--- ArrayReverse.java
+++ ArrayReverse.java
@@ -2,24 +2,24 @@ package HacktoberFest2022;
 
 public class ArrayReverse {
 
-	static void reverse(int a[], int a) 
+	static void reverse(int arr[], int a) 
 	{ 
-		int[] b = new int[n]; 
-		int j = n; 
+		int[] b = new int[a]; 
+		int j = a; 
 		
-		for (int i = 0; i < n; i++) { 
-			b[j - 1] = a[i]
-			j = j - 1
+		for (int i = 0; i < a; i++) { 
+			b[j - 1] = arr[i];
+			j = j - 1;
 		} 
 		System.out.println("Reversed array is: \n"); 
-		for (int k = 0; k < n; k++) { 
-			System.out.println(b[k]) 
+		for (int k = 0; k < a; k++) { 
+			System.out.println(b[k]);
 		} 
 	} 
 
 	public static void main(String[] args) 
 	{ 
 		int [] arr = {10, 20, 30, 40, 50}; 
-		reverse(abc, arr.length); 
+		reverse(arr, arr.length); 
 	} 
 } 
