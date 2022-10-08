f411ecd987612ba6a5a9a756e329cbcf45e58d8b
Shenu98 <91783648+Shenu98@users.noreply.github.com>
2022-10-08 22:28:07 +0530

    Update ArrayReverse.java

--- ArrayReverse.java
+++ ArrayReverse.java
@@ -1,25 +1,28 @@
+//fix the issues in the code
+
 package HacktoberFest2022;
 
 public class ArrayReverse {
-
-	static void reverse(int a[], int a) 
+//reverse function
+	static void reverse(int a[], int n) 
 	{ 
 		int[] b = new int[n]; 
 		int j = n; 
 		
 		for (int i = 0; i < n; i++) { 
-			b[j - 1] = a[i]
-			j = j - 1
+			b[j - 1] = a[i]; 
+			j = j - 1; 
 		} 
+		//display reverse order
 		System.out.println("Reversed array is: \n"); 
-		for (int k = 0; k < n; k++) { 
-			System.out.println(b[k]) 
+		for (int p = 0; p < n; p++) { 
+			System.out.println(b[k]); 
 		} 
 	} 
-
+//main function
 	public static void main(String[] args) 
 	{ 
 		int [] arr = {10, 20, 30, 40, 50}; 
-		reverse(abc, arr.length); 
+		reverse(arr, arr.length); 
 	} 
 } 
