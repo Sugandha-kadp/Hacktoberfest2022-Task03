a591d06659dfa5ddea69915730f9aea4d1c538b9
Sugandha <72730060+Sugandha-kadp@users.noreply.github.com>
2022-10-08 19:48:30 +0530

    Create ArrayReverse.java

---
+++ ArrayReverse.java
@@ -0,0 +1,25 @@
+package HacktoberFest2022;
+
+public class ArrayReverse {
+
+	static void reverse(int a[], int a) 
+	{ 
+		int[] b = new int[n]; 
+		int j = n; 
+		
+		for (int i = 0; i < n; i++) { 
+			b[j - 1] = a[i]
+			j = j - 1
+		} 
+		System.out.println("Reversed array is: \n"); 
+		for (int k = 0; k < n; k++) { 
+			System.out.println(b[k]) 
+		} 
+	} 
+
+	public static void main(String[] args) 
+	{ 
+		int [] arr = {10, 20, 30, 40, 50}; 
+		reverse(abc, arr.length); 
+	} 
+} 
